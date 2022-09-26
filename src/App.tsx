df547b3b1d0d24e08872e7e562c5bfe5af14f1c4
yerin512 <sosu0512@naver.com>
2022-09-26 15:38:07 +0900

    eslint와 prettierrc를 설정하라

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>메뉴 고르기</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
